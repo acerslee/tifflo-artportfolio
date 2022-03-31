e496c8d39dc6e2087df8d5b6a1003b2ff701612f
Alex Lee <alexlee@Alexs-MBP.fios-router.home>
2022-03-30 23:10:39 -0400

    initialized project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import SEO from '../components/seo'
+// import Navbar from '../components/navbar'
+// import Footer from '../components/footer'
+import { NextPage } from 'next'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type AppProps<P = any> = {
+  Component: P
+  pageProps: P
+  router: P
+}
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps, router }) => {
+  return (
+    // <SEO path = {router.asPath}/>
+    // <Navbar />
+    <Component {...pageProps} />
+    // <Footer />
+  )
 }
 
 export default MyApp
